4e5850f96adde57f39c3d6fd0008113d4676cd77
H184600 <184600@stud.hvl.no>
2024-10-28 14:42:34 +0100

    oppgave tekst

--- src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
+++ src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
@@ -1,6 +1,7 @@
 package no.hvl.dat100.oppgave4;
 
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.PrintWriter;
 
 import no.hvl.dat100.common.TODO;
@@ -10,6 +11,17 @@ public class SkrivBlogg {
 
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
 
-		throw new UnsupportedOperationException(TODO.method());
+String filbane = mappe + "/" + filnavn; 
+        
+        try {
+            FileWriter writer = new FileWriter(filbane); 
+            writer.write(samling.toString()); 
+            writer.close();
+            return true; 
+        } catch (Exception e) { 
+            return false; 
+        }
+    }
+
 	}
-}
+
